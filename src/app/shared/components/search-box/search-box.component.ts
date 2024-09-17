883204f87205e2f504a0b93f4fa41a6ffbaee63a
StefDmz <andrea.rosales@laureate.mx>
2024-09-17 14:00:22 -0600

    MOD Searchbox component

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,15 +1,35 @@
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html'
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+  //Subject: tipo especial de Observable, se puede hacer todo con lo que hacemos los observables
+  private debouncer: Subject<string> = new Subject<string>();
+
   @ViewChild('txtInput') txtInput!: ElementRef<HTMLInputElement>;
 
   @Output() public onValue: EventEmitter<string> = new EventEmitter();
+  
+  @Output() public onDebounce: EventEmitter<string> = new EventEmitter();
 
   @Input() public placeholder: string = '';
+
+  ngOnInit(): void {
+    this.debouncer
+    .pipe(
+      debounceTime(300)
+    )
+    .subscribe(value => {
+      this.onDebounce.emit(value);
+    });
+  }
+
+  onKeyPress(searchTerm: string) { 
+    this.debouncer.next(searchTerm);
+  }
 
   public getInputText(): void { 
     const input = this.txtInput.nativeElement.value;
